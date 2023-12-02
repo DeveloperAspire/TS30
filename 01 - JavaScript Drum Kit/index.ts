8565119e19f55944f023c0ec7c3f3634add07430
Franklin Okolie <franklinokolie86@gmail.com>
2023-12-02 13:10:21 +0100

    feat: worked on drumkit and wrote typescript for it

---
+++ 01 - JavaScript Drum Kit/index.ts
@@ -0,0 +1,47 @@
+"use strict";
+
+class DrumKit {
+  keys: NodeList;
+
+  constructor() {
+    this.keys = document.querySelectorAll(".key");
+
+    this.addEventListeners();
+  }
+
+  addEventListeners() {
+    window.addEventListener("keydown", (e) => {
+      const keyCode = e.key.toUpperCase().charCodeAt(0);
+      console.log(e);
+      this.keys.forEach((key: any) => {
+        if (+key.dataset.key !== keyCode) return;
+
+        this.playSound(keyCode, key);
+      });
+    });
+  }
+
+  playSound(keyCode: number, keyElement: HTMLDivElement) {
+    const audio: HTMLAudioElement | null = document.querySelector(
+      `audio[data-key="${keyCode}"]`
+    );
+
+    keyElement.classList.add("playing");
+
+    if (!audio) return;
+
+    audio.currentTime = 0;
+
+    audio.play();
+
+    this.removeStyling(keyElement);
+  }
+
+  removeStyling(keyElement: HTMLDivElement) {
+    keyElement.addEventListener("transitionend", () => {
+      keyElement.classList.remove("playing");
+    });
+  }
+}
+
+new DrumKit();
